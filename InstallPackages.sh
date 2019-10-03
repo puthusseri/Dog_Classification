98513c33d45d8ab18dbb1eb0e3eae387894a248c
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2019-10-03 16:36:01 +0530

    docs: Updated the readme Also included the requiremet files.

---
+++ InstallPackages.sh
@@ -0,0 +1,13 @@
+sudo apt install curl
+sudo apt-get install python3-distutils
+conda install -c anaconda setuptools
+pip install tqdm
+pip install torch
+pip install torchvision
+sudo apt  install jupyter-core
+pip install jupyter
+pip install opencv-python
+
+
+
+
